f95edfc85ee017c5b8e3626f2b53339f1ca634ad
johnmuthua <caveomuthama@gmail.com>
2024-04-11 23:40:36 +0300

    Add type definations

---
+++ src/resolvers.ts
@@ -0,0 +1,8 @@
+import { Resolvers } from "./types";
+export const resolvers: Resolvers = {
+  Query: {
+    featuredPlaylists: (_, __, { dataSource }) => {
+      return dataSource.spotifyAPI.getFeaturedPlaylists();
+    },
+  },
+};
