182927056c851b251c7c1f20acb94395a704af6f
johnmuthua <caveomuthama@gmail.com>
2024-04-12 11:27:10 +0300

    chore: cleared a typo

--- src/datasources/spotify-api.ts
+++ src/datasources/spotify-api.ts
@@ -1,10 +1,10 @@
 import { Playlist } from "../types";
 import { RESTDataSource } from "@apollo/datasource-rest";
 
-export class Spotify extends RESTDataSource {
+export class SpotifyAPI extends RESTDataSource {
   baseURL = "https://spotify-demo-api-fe224840a08c.herokuapp.com/v1/";
   async getFeaturedPlaylists(): Promise<Playlist[]> {
-    const response = await this.get<{ playlists: { items: Playlist } }>(
+    const response = await this.get<{ playlists: { items: Playlist[] } }>(
       "browse/featured-playlists"
     );
 
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,8 +1,8 @@
 import { Resolvers } from "./types";
 export const resolvers: Resolvers = {
   Query: {
-    featuredPlaylists: (_, __, { dataSource }) => {
-      return dataSource.spotifyAPI.getFeaturedPlaylists();
+    featuredPlaylists: (_, __, { dataSources }) => {
+      return dataSources.spotifyAPI.getFeaturedPlaylists();
     },
   },
 };
